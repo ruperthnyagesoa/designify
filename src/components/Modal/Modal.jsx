3c979fe1c38fd6a9e4d7bb9bcf73a2a765aff4c2
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-15 13:20:40 +0300

    overflow

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,73 @@
+import React, { useRef } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import { Background, CloseModalButton, ModalWrapper } from './ModalStyles';
+import Form from '../Form/Form';
+
+const Modal = ({ showModal, toggleModal }) => {
+	const modalRef = useRef();
+
+	const closeModal = (e) => {
+		if (modalRef.current === e.target) {
+			toggleModal();
+		}
+	};
+
+	const backgroundVariants = {
+		initial: {
+			opacity: 0,
+		},
+		animate: {
+			opacity: 1,
+			transition: {
+				duration: 0.4,
+			},
+		},
+	};
+
+	const modalVariants = {
+		initial: {
+			opacity: 0,
+			y: 200,
+		},
+		animate: {
+			opacity: 1,
+			y: 0,
+			transition: {
+				duration: 0.4,
+				type: 'spring',
+				stiffness: 100,
+			},
+		},
+		exit: {
+			y: -200,
+			opacity: 0,
+		},
+	};
+
+	return (
+		<AnimatePresence>
+			{showModal && (
+				<Background
+					variants={backgroundVariants}
+					animate='animate'
+					initial='initial'
+					onClick={closeModal}
+					ref={modalRef}
+					exit={{ opacity: 0 }}
+				>
+					<ModalWrapper
+						variants={modalVariants}
+						animate='animate'
+						initial='initial'
+						exit={{ opacity: 0, y: '-100vh' }}
+					>
+						<Form></Form>
+						<CloseModalButton aria-label='Close modal' onClick={toggleModal} />
+					</ModalWrapper>
+				</Background>
+			)}
+		</AnimatePresence>
+	);
+};
+
+export default Modal;
