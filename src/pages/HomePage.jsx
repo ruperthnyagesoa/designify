c33c5e7b2f14d59f9a18775b725e7d2e2f2f8cc6
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-15 10:01:16 +0300

    pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Features from '../components/Features/Features';
+import Hero from '../components/Hero/Hero';
+
+const HomePage = () => {
+	return (
+		<>
+			<Hero />
+			<Features />
+		</>
+	);
+};
+
+export default HomePage;
